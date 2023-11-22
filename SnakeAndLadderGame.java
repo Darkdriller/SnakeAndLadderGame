d64147ac115fa4af11aac6aca33a9c7766d87749
Exypnos <swaindhruv28@gmail.com>
2023-11-22 15:20:01 +0530

    Refactored: Completed Use case 2

--- SnakeAndLadderGame.java
+++ SnakeAndLadderGame.java
@@ -1,7 +1,11 @@
 public class SnakeAndLadderGame {
 	public static void main(String[] args) {
 		int playerPos = 0;
+                int dice = 0;
 		System.out.println("Snake And Ladder Game");
 		System.out.println("Starting Position of the Player = " + playerPos);
+                dice =  (int) (Math.random() * 6 ) + 1;
+                System.out.println("Roll:  " +  dice);
+
 	}
 }
