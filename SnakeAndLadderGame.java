18b05d9a931d0baae9b03ee21c5892d32064eab4
Exypnos <swaindhruv28@gmail.com>
2023-11-22 15:09:30 +0530

    Added SnakeAndLadderGame.java and .gitignore

---
+++ SnakeAndLadderGame.java
@@ -0,0 +1,7 @@
+public class SnakeAndLadderGame {
+	public static void main(String[] args) {
+		int playerPosition = 0;
+		System.out.println("Welcome to the Game of SNAKE & LADDER");
+		System.out.println("Starting Position of the Player = " + playerPosition);
+	}
+}
